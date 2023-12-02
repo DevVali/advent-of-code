0db554e33d0602389c02434da8c4ad92dc97922d
DevVali <90271108+DevVali@users.noreply.github.com>
2023-12-02 10:32:54 +0100

    Improve 01.php

--- 2023/01.php
+++ 2023/01.php
@@ -4,35 +4,44 @@ $input = file(__DIR__."/01_input.txt");
 
 // FIRST PART
 
-$calibration = (int) 0;
+function part1($input) {
+    $calibration = (int) 0;
+    
+    foreach ($input as $line) {
+        $filteredDigits = array_filter(preg_split("/\D+/", $line));
 
-foreach ($input as $line) {
-    $filteredDigits = array_filter(preg_split("/\D+/", $line));
+        $firstDigit = (int) substr(reset($filteredDigits), 0, 1);
+        $lastDigit = (int) substr(end($filteredDigits), -1);
 
-    $firstDigit = (int) substr(reset($filteredDigits), 0, 1);
-    $lastDigit = (int) substr(end($filteredDigits), -1);
+        $sum = (int) ($firstDigit . $lastDigit);
+        $calibration += $sum;
+    }
 
-    $sum = (int) ($firstDigit . $lastDigit);
-    $calibration += $sum;
+    return $calibration;
 }
-
-echo $calibration."\n";
 
 // SECOND PART
 
-$calibration = (int) 0;
+function part2($input) { 
+    $calibration = (int) 0;
+    
+    foreach ($input as $line) {
+        $values = str_replace(["oneight", "twone", "eightwo"], [18, 21, 82], $line);
+        $values = str_replace(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], [1, 2, 3, 4, 5, 6, 7, 8, 9], $values);
 
-foreach ($input as $line) {
-    $values = str_replace(["oneight", "twone", "eightwo"], [18, 21, 82], $line);
-    $values = str_replace(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"], [1, 2, 3, 4, 5, 6, 7, 8, 9], $values);
+        $filteredDigits = array_filter(preg_split("/\D+/", $values));
 
-    $filteredDigits = array_filter(preg_split("/\D+/", $values));
+        $firstDigit = (int) substr(reset($filteredDigits), 0, 1);
+        $lastDigit = (int) substr(end($filteredDigits), -1);
 
-    $firstDigit = (int) substr(reset($filteredDigits), 0, 1);
-    $lastDigit = (int) substr(end($filteredDigits), -1);
+        $sum = (int) ($firstDigit . $lastDigit);
+        $calibration += $sum;
+    }
 
-    $sum = (int) ($firstDigit . $lastDigit);
-    $calibration += $sum;
+    return $calibration;
 }
 
-echo $calibration;
+// OUTPUT
+
+print(part1($input)."\n");
+print(part2($input));
